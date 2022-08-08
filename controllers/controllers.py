bc8f2befbe76c34e936fb00bf78a98654fe632d4
robin-cybrosys <robin.cybrosys@gmail.com>
2022-08-08 18:34:31 +0530

    on going appointment creation line 24 controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -17,22 +17,43 @@ class WebsiteForm(http.Controller):
 class WebsiteAccount(http.Controller):
     @http.route(['/appointment_submit'], type='http', auth='public',
                 website=True)
-    def my_controller_method(self, redirect=None, **post):
-        print("laalalalaalaa")
+    def appointment_submit(self, **post):
+        patient_card = post['patient_id']
+        print(patient_card)
         # Put your backend operations here, e.g.
         appointments = request.env['hospital.appointment'].sudo().search([])
+        appointments.create({
+            'patient_card_id': post.get('patient_id'),
+            # 'patient_id': 'New',
+            'doctor_id': 21,
+            # patient_card = post['patient_id']
+            # print(patient_card)
 
+            # 'name': post.get('name'),
+            # 'email': post.get('email'),
+            # 'phone': post.get('phone')
+        })
+        # vals = {
+        #     'appointments': appointments,
+        # }
+        # inherited the model to pass the values to the model from the form#
+        # return request.render(vals)
+        # finally send a request to render the thank you page#
+        # appointments.create({
+        #
+        #     'patient_card_id': self.browse(patient_card),
+        # 'patient_id': 'New',
+        #     'doctor_id': 21,
+        # 'team_id': cls.sales_team_1.id,
+        # })
+        # print(each,"laalalalaalaa")
 
-
-        # demo
-        # products = request.env['product.template'].sudo().search([])
-        # for each in products:
         #     each.qty_sold = 0
         #     each.views = 0
         #     each.top_selling = False
         #
-        # date = fields.Datetime.now()
-        # date_before = date - datetime.timedelta(days=7)
+        #     date = fields.Datetime.now()
+        #     date_before = date - datetime.timedelta(days=7)
         #
         # orders = request.env['sale.order'].sudo().search(
         #     [('date_order', '<=', date),
@@ -57,3 +78,37 @@ class WebsiteAccount(http.Controller):
         # website_product_ids.top_selling = True
         # return request.render('website.homepage',
         #                       {'website_product_ids': website_product_ids})
+
+    # demo
+    # products = request.env['product.template'].sudo().search([])
+    # for each in products:
+    #     each.qty_sold = 0
+    #     each.views = 0
+    #     each.top_selling = False
+    #
+    # date = fields.Datetime.now()
+    # date_before = date - datetime.timedelta(days=7)
+    #
+    # orders = request.env['sale.order'].sudo().search(
+    #     [('date_order', '<=', date),
+    #      ('date_order', '>=', date_before), ('website_id', '!=', False),
+    #      ('state', 'in', ('sale', 'done'))])
+    # for order in orders:
+    #     order_line = order.order_line
+    # for product in order_line:
+    #     print(product.product_id.qty_sold)
+    #     product.product_id.qty_sold = product.product_id.qty_sold + 1
+    #     products = request.env['website.track'].sudo().search(
+    #         [('visit_datetime', '<=', date),
+    #          ('visit_datetime', '>=', date_before),
+    #          ('product_id', '!=', False)])
+    # for pro in products:
+    #     pro.product_id.views = pro.product_id.views + 1
+    # super(WebsiteSort, self).index()
+    # website_product_ids = request.env['product.template'].sudo().search(
+    #     [('is_published', '=', True),
+    #      ('qty_sold', '!=', 0)],
+    #     order='qty_sold desc', limit=4)
+    # website_product_ids.top_selling = True
+    # return request.render('website.homepage',
+    #                       {'website_product_ids': website_product_ids})
